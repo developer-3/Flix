c014498046f6679c47be3f07c7e5f1b20886aece
Adam Anderson <adamcanderson400@gmail.com>
2020-01-31 20:41:55 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Adam Anderson on 1/31/20.
+//  Copyright © 2020 adamcanderson400. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
