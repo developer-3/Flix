cc52427e75d2907fdf6725d87f53628c070e12b5
Adam Anderson <adamcanderson400@gmail.com>
2020-02-12 20:26:19 -0500

    Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Adam Anderson on 2/12/20.
+//  Copyright © 2020 adamcanderson400. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
